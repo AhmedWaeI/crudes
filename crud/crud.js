a846dd3ae1fc6aa3924223c3f2333899e271e3bb
xSlayer <ahmed_the_best17@yahoo.com>
2023-11-21 22:57:39 +0200

    Search function

--- crud/crud.js
+++ crud/crud.js
@@ -55,3 +55,24 @@ function deleteindex(index)
     localStorage.setItem("Product",JSON.stringify(productcontainer));
 
 }
+function search(term)
+{
+var tablerow="";
+for(var i=0;i<productcontainer.length;i++)
+{
+    if(productcontainer[i].name.toLowerCase().includes(term.toLowerCase()))
+    {
+        tablerow += `<tr class="border-b border-slate-300">
+        <td class=" px-4 py-4">${i}</td>
+        <td class=" px-4 py-4">${productcontainer[i].name}</td>
+        <td class=" px-4 py-4">${productcontainer[i].price}</td>
+        <td class=" px-4 py-4">${productcontainer[i].category}</td>
+        <td class=" px-4 py-4">${productcontainer[i].description}</td>
+        <td class=" px-4 py-4"><button onclick="deleteindex(${i});display()" class= "p-1.5 text-red-600 border border-red-600 rounded-md hover:bg-red-600 hover:text-slate-50">Delete</button></td>
+        <td class=" px-4 py-4"><button class= "p-1.5 text-green-600 border border-green-600  rounded-md hover:bg-green-600 hover:text-slate-50">Update</button></td>
+        </tr>`
+    }
+}
+document.getElementById("producttable").innerHTML=tablerow;
+
+}
